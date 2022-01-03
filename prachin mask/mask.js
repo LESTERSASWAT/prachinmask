80667c77bc1e7d186ddbe90b7e3f3356a27d368b
LESTERSASWAT <90452182+LESTERSASWAT@users.noreply.github.com>
2022-01-04 00:04:36 +0530

    Add files via upload

---
+++ prachin mask/mask.js
@@ -0,0 +1,37 @@
+var con="Coronavirus disease (COVID-19) is an infectious disease caused by the SARS-CoV-2 virus.Most people who fall sick with COVID-19 will experience mild to moderate symptoms and recover without special treatment. However, some will become seriously ill and require medical attention."
+function nextbolo(){
+console.log(con)
+
+}
+function spread(){
+    console.log(gjg)
+}
+var gjg="The virus can spread from an infected person’s mouth or nose in small liquid particles when they cough, sneeze, speak, sing or breathe. These particles range from larger respiratory droplets to smaller aerosols.You can be infected by breathing in the virus if you are near someone who has COVID-19, or by touching a contaminated surface and then your eyes, nose or mouth. The virus spreads more easily indoors and in crowded settings. "
+function start(){
+    console.log(star)
+}
+var star="The first case of COVID-19 was reported Dec. 1, 2019, and the cause was a then-new coronavirus later named SARS-CoV-2. SARS-CoV-2 may have originated in an animal and changed (mutated) so it could cause illness in humans. In the past, several infectious disease outbreaks have been traced to viruses originating in birds, pigs, bats and other animals that mutated to become dangerous to humans. Research continues, and more study may reveal how and why the coronavirus evolved to cause pandemic disease."
+function period(){
+console.log(peerr)
+}
+var peerr="Symptoms show up in people within two to 14 days of exposure to the virus. A person infected with the coronavirus is contagious to others for up to two days before symptoms appear, and they remain contagious to others for 10 to 20 days, depending upon their immune system and the severity of their illness. "
+var desl="COVID-19 is diagnosed through a laboratory test. Diagnosis by examination alone is difficult since many COVID-19 signs and symptoms can be caused by other illnesses. Some people with the coronavirus do not have symptoms at all."
+function desp(){
+    console.log(desl)
+}
+function treat(){
+    console.log(tra)
+}
+var tra="Treatment for COVID-19 addresses the signs and symptoms of the infection and supports people with more severe disease. For mild cases of coronavirus disease, your doctor may recommend measures such as fever reducers or over-the-counter medications. More severe cases may require hospital care, where a patient may receive a combination of treatments that could include steroids, oxygen, mechanical breathing support and other COVID-19 treatments in development. Infusions of monoclonal antibodies given to certain patients early in the infection may reduce the symptoms, severity and duration of the illness."
+var sad="SARS stands for severe acute respiratory syndrome. In 2003, an outbreak of SARS affected people in several countries before ending in 2004. The coronavirus that causes COVID-19 is similar to the one that caused the 2003 SARS outbreak.Since the 2019 coronavirus is related to the original coronavirus that caused SARS and can also cause severe acute respiratory syndrome, there is “SARS” in its name: SARS-CoV-2. Much is still unknown about these viruses, but SARS-CoV-2 spreads faster and farther than the 2003 SARS-CoV-1 virus. This is likely because of how easily it is transmitted person to person, even from asymptomatic carriers of the virus."
+function sars(){
+    console.log(sad)
+}
+var dy="    *Clean your hands before you put your mask on, as well as before and after you take it off, and after you touch it at any time.*Make sure it covers both your nose, mouth and chin.*When you take off a mask, store it in a clean plastic bag, and every day either wash it if it’s a fabric mask, or dispose of a medical mask in a trash bin.*Don’t use masks with valves."
+
+
+function howdy(){
+ console.log(dy)
+}
+
+
